d00823b90d145d7864bc42b2b5f46457ddad96a1
premkumar8686 <premkumarweb0369@gmail.com>
2023-05-05 23:10:34 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useId } from "react";
 
 function App() {
+  const id = useId();
+  const id1 = useId();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <h1>{id}</h1>
+        <h1>{id + 'First'}</h1>
+        <h1>{id1}</h1>
+    </>
   );
 }
 
